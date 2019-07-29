b3cfe1daf5fae8a2e3b4255dbe9512a69982b010
michaelbravo777 <michael.bravo777@gmail.com>
2019-07-29 17:03:45 -0400

    added separate chaining

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -20,7 +20,7 @@ function main() {
     { "Ent": "Treebeard" }
   ]
 
-  
+
 
   let stringHash = new Map()
 
@@ -49,19 +49,19 @@ isPalindrome = (string) => {
 
 includesPalindrome = (string) => {
   let newMap = new Map()
-  for (let i = 0; i < string.length; i ++) {
+  for (let i = 0; i < string.length; i++) {
     if (!newMap.has(string[i])) {
       newMap.set(string[i], 1)
     } else {
-    let temp  = newMap.get(string[i]) + 1
-    newMap.set(string[i], temp)
+      let temp = newMap.get(string[i]) + 1
+      newMap.set(string[i], temp)
     }
   }
   let odd = 0;
   console.log(newMap.values())
   Array.from(newMap.values()).forEach(value => {
     if (value % 2 !== 0) {
-      odd +=1
+      odd += 1
     }
   })
   if (odd > 1) {
@@ -70,15 +70,31 @@ includesPalindrome = (string) => {
   return true;
 }
 
+checkAnagrams = (array) => {
+  let anagramHash = new Map()
+  let newArray = []
+  let wordValue = 0
+  array.forEach(word => {
+    let sortedWord = word.split('').sort().join('')
+    if (!anagramHash.has(sortedWord)) {
+      anagramHash.set(sortedWord, wordValue)
+      newArray.push([word])
+      wordValue++
+    }
+    else {
+      const newArrayPosition = anagramHash.get(sortedWord);
+      newArray[newArrayPosition].push(word);
+    }
+  })
+  return newArray;
+}
 
-console.log(includesPalindrome('racecard'))
+// console.log(includesPalindrome('racecard'))
 
- 
+let anagram_input = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']
+console.log(checkAnagrams(anagram_input))
 
-//check if string is even 
-    // odd even
 
-    // odd code
 
 main()
 
